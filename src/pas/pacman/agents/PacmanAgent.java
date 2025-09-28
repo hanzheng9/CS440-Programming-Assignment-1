9612f39291ed28f42e9eb64e492a0458e08fedb2
Han Zheng <hanzheng@Hans-MacBook-Air-2.local>
2025-09-28 14:05:02 -0400

    current progress

--- src/pas/pacman/agents/PacmanAgent.java
+++ src/pas/pacman/agents/PacmanAgent.java
@@ -1,6 +1,9 @@
 package src.pas.pacman.agents;
 
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 // SYSTEM IMPORTS
 import java.util.Random;
 import java.util.Set;
@@ -16,14 +19,13 @@ import edu.bu.pas.pacman.graph.Path;
 import edu.bu.pas.pacman.graph.PelletGraph.PelletVertex;
 import edu.bu.pas.pacman.utils.Coordinate;
 import edu.bu.pas.pacman.utils.Pair;
-
-
 public class PacmanAgent
     extends SearchAgent
     implements ThriftyPelletEater
 {
 
     private final Random random;
+    private Coordinate goal;
 
     public PacmanAgent(int myUnitId,
                        int pacmanId,
@@ -38,8 +40,7 @@ public class PacmanAgent
     @Override
     public Set<PelletVertex> getOutoingNeighbors(final PelletVertex vertex,
                                                  final GameView game)
-   {
-
+    {
         Set<PelletVertex> coords = new HashSet<>();
         String[] directions =  {"NORTH", "SOUTH", "EAST", "WEST"};
          for(int i = 0; i < 4; i++){
@@ -48,6 +49,7 @@ public class PacmanAgent
         }
         return coords;
     }
+
     @Override
     public float getEdgeWeight(final PelletVertex src,
                                final PelletVertex dst)
@@ -59,7 +61,9 @@ public class PacmanAgent
     public float getHeuristic(final PelletVertex src,
                               final GameView game)
     {
-        return 1f;
+        int dx = Math.abs(src.getX() - goal.getX());
+        int dy = Math.abs(src.getY() - goal.getY());
+        return dx + dy;
     }
 
     @Override
@@ -80,6 +84,29 @@ public class PacmanAgent
                                         final Coordinate tgt,
                                         final GameView game)
     {
+        this.goal = tgt;
+        class Node 
+        {
+            final Coordinate c;
+            final float cost;    
+            final Node parent;
+
+            Node(Coordinate c, float cost, Node parent) {
+                this.c = c; 
+                this.cost = cost; 
+                this.parent = parent;
+            }
+        }
+        Comparator<Node> nodeComparator = new Comparator<Node>() 
+        {
+            @Override
+            public int compare(Node a, Node b) {
+                double totalA = a.cost + getHeuristic(a.c, tgt);
+                double totalB = b.cost + getHeuristic(b.c, tgt);
+                return Double.compare(totalA, totalB);
+            }
+        };
+        PriorityQueue<Node> open = new PriorityQueue<>(nodeComparator);
         return null;
     }
 
